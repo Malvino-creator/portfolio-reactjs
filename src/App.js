4f97a5dabebaa5bc7bfbb35b822c6f451962f2a4
Moringa School <malvin.opondo@student.moringaschool.com>
2022-11-17 01:11:24 +0300

    Create components folder structure

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
           <h1>MALVIN OPONDO</h1>
           <h2> Welcome to my Space!</h2>
-        </p>
+          <p> A FullStack Developer...
+            Good design makes a statement, and that's what sets me apart. My mediums are as bold, loud and proud. 
+          </p>
       </header>
     </div>
   );
