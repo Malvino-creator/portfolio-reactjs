b409e1a62c560d44535590e23c5370128a5214b8
Moringa School <malvin.opondo@student.moringaschool.com>
2022-11-18 14:26:15 +0300

    Debugging

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
           <h1>MALVIN OPONDO</h1>
           <h2> Welcome to my Space!</h2>
           <p> A FullStack Developer...
-            Good design makes a statement, and that's what sets me apart. My mediums are as bold, loud and proud. 
+            Good design makes a statement, ad that's what sets me apart. My mediums are as bold, loud and proud.
           </p>
+        </p>
       </header>
     </div>
   );
