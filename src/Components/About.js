db282873577e175c2c8230b337dbe460356aab09
Moringa School <malvin.opondo@student.moringaschool.com>
2022-11-28 02:49:03 +0300

    feat: This commit debugs and styles the home component

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,12 +5,9 @@ function About() {
   return (
     <div>
          <div id='about'>
-            <h1><bold>MALVIN OPONDO.</bold></h1>
-                <p> Welcome to my Space!</p>
-                <h1>A FullStack Developer</h1>
-                <p> I like dabbling in various parts of frontend development and like to learn about new tehnologies. 
-                <br></br>
-                <br></br>During my free time i love to search for inspiration of designs on Pinterest, Dribble.
+            <h1><bold>I am Malvin Opondo.</bold></h1>
+                <p> I am a FullStack Developer and Creative Designer.I like dabbling in various parts of frontend<br></br> development and like to learn about new tehnologies. 
+                During my free time i love to search for inspiration of designs on Pinterest, Dribble.<br></br>
                 I also enjoy travelling and meeting new people.
             </p>
         </div>
