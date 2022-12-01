c4c6d010fda77eaccacade4390ba9f949fdc65f2
Moringa School <malvin.opondo@student.moringaschool.com>
2022-12-01 11:55:14 +0300

    Deployment on Cloudflare

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,10 +5,10 @@ function About() {
   return (
     <div>
          <div id='about'>
-            <h1><bold>I am Malvin Opondo.</bold></h1>
+            <h1><bold>I am Malvin Opondo..</bold></h1>
                 <p> I am a FullStack Developer and Creative Designer.I like dabbling in various parts of frontend<br></br> development and like to learn about new tehnologies. 
-                During my free time i love to search for inspiration of designs on Pinterest, Dribble.<br></br>
-                I also enjoy travelling and meeting new people.
+                During my free time,<br></br> i love to browse for design insprations on Pinterest, Dribble.<br></br>
+                I also enjoy travelling and networking.
             </p>
         </div>
     </div>
